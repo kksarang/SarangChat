5f5bf106de51c5a1008bfe70c9f2087f7d3d02cd
sarangsvaro <sarang.svaro@gmail.com>
2021-07-20 18:50:28 +0530

    FIRST

---
+++ app/src/main/java/com/example/sarangconnect/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.sarangconnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent PhoneNumberpage= new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(PhoneNumberpage);
+                finish();
+            }
+        }, 3000);
+
+    }
+}
